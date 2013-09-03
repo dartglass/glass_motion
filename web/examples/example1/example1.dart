120951614315e3a9aefb5e0ca1b89f7b324b3b59
Ryan Weaving <rweaving@gmail.com>
2013-09-03 16:13:56 -0700

    Updated example with new values

--- web/examples/example1/example1.dart
+++ web/examples/example1/example1.dart
@@ -9,8 +9,9 @@ void main() {
   query("#sample_text_id")..text   = "Test";
   
   glassMotion.onMotionUpdate = ((e){
-    query("#sample_text_id")..text   = " Acceleration : "..appendText(glassMotion.acceleration.toStringAsFixed(3)); 
-    query("#sample_text_id2")..text   = " Movement : "..appendText(glassMotion.movement.toStringAsFixed(3)); 
+    query("#sample_text_id")..text   = " Acceleration : ${glassMotion.acceleration.toStringAsFixed(3)}"; 
+    query("#sample_text_id2")..text   = " Yaw (Head Side Tilt): ${glassMotion.yaw.toStringAsFixed(0)}°"; 
+    query("#sample_text_id3")..text   = " Avg Movement : ${glassMotion.movement.toStringAsFixed(3)}"; 
   });
 
 }
