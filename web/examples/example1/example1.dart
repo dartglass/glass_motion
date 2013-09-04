b75e39d712eed28f63b4fb729cd58b0d369dd778
Ryan Weaving <rweaving@gmail.com>
2013-09-04 11:58:58 -0700

    Added head tilt to example 1

--- web/examples/example1/example1.dart
+++ web/examples/example1/example1.dart
@@ -10,7 +10,7 @@ void main() {
   
   glassMotion.onMotionUpdate = ((e){
     query("#sample_text_id")..text   = " Acceleration : ${glassMotion.acceleration.toStringAsFixed(3)}"; 
-    query("#sample_text_id2")..text   = " Yaw (Head Side Tilt): ${glassMotion.yaw.toStringAsFixed(0)}°"; 
+    query("#sample_text_id2")..text   = " Yaw : ${glassMotion.yaw.toStringAsFixed(0)}° , Tilt: ${glassMotion.tilt.toStringAsFixed(0)}°"; 
     query("#sample_text_id3")..text   = " Avg Movement : ${glassMotion.movement.toStringAsFixed(3)}"; 
   });
 
