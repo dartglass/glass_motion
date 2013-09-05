8908a73ba8be573f9e3cafc3deeb06a2bf92049b
Ryan Weaving <rweaving@gmail.com>
2013-09-05 09:27:31 -0700

    Renamed example 2 to crosshair

--- web/examples/crosshair/crosshair.dart
+++ web/examples/crosshair/crosshair.dart
@@ -1,6 +1,4 @@
 import 'dart:html';
-import 'dart:math' as Math;
-
 import 'package:glass_motion/glass_motion.dart';
 
 void main() {
@@ -15,18 +13,15 @@ void main() {
   num tiltRangeMax = 30;
   
   testImage = new Element.tag('img');
-  testImage.src = "images/cross_hair.png";
+  testImage.src = "images/crosshair.png";
   
   glassMotion.onMotionUpdate = ((e){
 
     num xPos =  (glassMotion.yaw / (yawRangeMax-yawRangeMin)) * canvas.width - (testImage.width / 2) + (canvas.width / 2);
     num yPos =  (glassMotion.tilt / (tiltRangeMax-tiltRangeMin)) * canvas.height - (testImage.height / 2) + (canvas.height / 2);
   
-//    print("$xPos,$yPos  ${glassMotion.tilt},${glassMotion.yaw}");
-
     canvas.context2D.clearRect(0,0,canvas.width,canvas.height);
-    canvas.context2D.drawImage(testImage, xPos, yPos);
-    
+    canvas.context2D.drawImage(testImage, xPos, yPos);    
   });
 
 }
