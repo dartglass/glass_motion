d55be382d13265c2c6ba81bdbbbf85238e3f09da
Ryan Weaving <rweaving@gmail.com>
2013-09-04 19:46:42 -0700

    New example using tilt and yaw with cross hair

--- web/examples/example2/example2.dart
+++ web/examples/example2/example2.dart
@@ -1,4 +1,5 @@
 import 'dart:html';
+import 'dart:math' as Math;
 
 import 'package:glass_motion/glass_motion.dart';
 
@@ -8,17 +9,24 @@ void main() {
   CanvasElement canvas = document.query('#canvas');
   ImageElement testImage;
  
+  num yawRangeMin = -30;
+  num yawRangeMax = 30;
+  num tiltRangeMin = -30;
+  num tiltRangeMax = 30;
+  
   testImage = new Element.tag('img');
-  testImage.src = "images/and.png";
+  testImage.src = "images/cross_hair.png";
   
   glassMotion.onMotionUpdate = ((e){
-    num destX = canvas.width/2;
-    num destY = canvas.height/2; 
-    
-    num offset = (glassMotion.yaw/60)*destX;
-    
+
+    num xPos =  (glassMotion.yaw / (yawRangeMax-yawRangeMin)) * canvas.width - (testImage.width / 2) + (canvas.width / 2);
+    num yPos =  (glassMotion.tilt / (tiltRangeMax-tiltRangeMin)) * canvas.height - (testImage.height / 2) + (canvas.height / 2);
+  
+//    print("$xPos,$yPos  ${glassMotion.tilt},${glassMotion.yaw}");
+
     canvas.context2D.clearRect(0,0,canvas.width,canvas.height);
-    canvas.context2D.drawImage(testImage, destX + offset, destY);
+    canvas.context2D.drawImage(testImage, xPos, yPos);
+    
   });
 
 }
