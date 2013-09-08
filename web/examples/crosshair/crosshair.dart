e6a724decd09a24b93bedfae9bbc82a8f70811b1
Ryan Weaving <rweaving@gmail.com>
2013-09-08 15:10:58 -0700

    Added smaller cursors, and cursor chances color above motion threshold

--- web/examples/crosshair/crosshair.dart
+++ web/examples/crosshair/crosshair.dart
@@ -5,23 +5,47 @@ void main() {
   
   GlassMotion glassMotion = new GlassMotion(window);
   CanvasElement canvas = document.query('#canvas');
-  ImageElement testImage;
+  
+  ImageElement crosshairGreen;
+  ImageElement crosshairRed;
+  ImageElement crosshairWhite;
  
   num yawRangeMin = -30;
   num yawRangeMax = 30;
   num tiltRangeMin = -30;
   num tiltRangeMax = 30;
   
-  testImage = new Element.tag('img');
-  testImage.src = "images/crosshair.png";
+  num yaw = 0;
+  num tilt = 0;
+  
+  num xPos = 0;
+  num yPos = 0;
+  
+  crosshairGreen = new Element.tag('img');
+  crosshairGreen.src = "images/crosshair_green.png";
+  
+  crosshairRed = new Element.tag('img');
+  crosshairRed.src = "images/crosshair_red.png";
+  
+  crosshairWhite = new Element.tag('img');
+  crosshairWhite.src = "images/crosshair_white.png";
   
   glassMotion.onMotionUpdate = ((e){
-
-    num xPos =  (glassMotion.yaw / (yawRangeMax-yawRangeMin)) * canvas.width - (testImage.width / 2) + (canvas.width / 2);
-    num yPos =  (glassMotion.tilt / (tiltRangeMax-tiltRangeMin)) * canvas.height - (testImage.height / 2) + (canvas.height / 2);
+    yaw = glassMotion.yaw * 0.2 + yaw * 0.8;
+    tilt = glassMotion.tilt * 0.2 + tilt * 0.8; 
+    
+    xPos =  ((glassMotion.yaw / (yawRangeMax-yawRangeMin)) * canvas.width - (crosshairGreen.width / 2) + (canvas.width / 2)).clamp(- (crosshairGreen.width / 2), canvas.width- (crosshairGreen.width / 2));
+    yPos =  ((glassMotion.tilt / (tiltRangeMax-tiltRangeMin)) * canvas.height - (crosshairGreen.height / 2) + (canvas.height / 2)).clamp(- (crosshairGreen.width / 2), canvas.height- (crosshairGreen.width / 2));
   
     canvas.context2D.clearRect(0,0,canvas.width,canvas.height);
-    canvas.context2D.drawImage(testImage, xPos, yPos);    
+    
+    if(glassMotion.movement > 2){
+        canvas.context2D.drawImage(crosshairGreen, xPos, yPos);
+    }
+    else{
+      canvas.context2D.drawImage(crosshairRed, xPos, yPos);
+    }
+    
   });
 
 }
