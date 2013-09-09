e2c0c0ad8cd077e58213f6a0c4a27f9f766be205
Ryan Weaving <rweaving@gmail.com>
2013-09-08 17:59:19 -0700

    Remove gravity compensation function

--- lib/src/motion_handler.dart
+++ lib/src/motion_handler.dart
@@ -2,14 +2,12 @@ part of glass_motion;
 
 class MotionHandler{
   
-  static const double timeConstant = 500.0; // For gravity Compensation update intervals > then timeConstant will correct faster
   static const avgMovementSampleSize = 20;
   static const tweekyVectorThreshold = 10;
   
-  double _alpha;
   List<double> accelerationHistory;
   
-  double get acceleration => _gravityComp.length;
+  double get acceleration => accelerationVector.length;
   double get roll => Math.atan(accelerationVectorNormalized.x/(-accelerationVectorNormalized.y))*radians2degrees;
   double get pitch => Math.atan(accelerationVectorNormalized.z/(-accelerationVectorNormalized.y))*radians2degrees;
   //double get pitch => Math.atan((accelerationVectorNormalized.x*accelerationVectorNormalized.x + (accelerationVectorNormalized.y)*(accelerationVectorNormalized.y))/accelerationVectorNormalized.z)*radians2degrees;
@@ -19,27 +17,17 @@ class MotionHandler{
   int _skipCount;
   
   Vector3 accelerationVector;
-  Vector3 accelerationVectorPrevious;
   Vector3 accelerationVectorDelta;
-  Vector3 accelerationVectorDeltaSum;
   Vector3 accelerationVectorNormalized;
-    
-  Vector3 accelerationVectorGravityCompensated;
-  Vector3 _gravityComp; // Gravity Compensation vector
   Vector3 _vector;
-  Vector3 accelerationVectorGravityCompOLD;
    
   MotionHandler(){
     _previousTimestamp = 0;
     accelerationHistory = new List<double>();
     
     accelerationVector = new Vector3(0.0, 0.0, 0.0);
-    accelerationVectorPrevious = new Vector3(0.0, 0.0, 0.0);
-    accelerationVectorNormalized = new Vector3(0.0, 0.0, 0.0);
-    accelerationVectorGravityCompensated = new Vector3(0.0, 0.0, 0.0);
     accelerationVectorDelta = new Vector3(0.0, 0.0, 0.0);
-    accelerationVectorDeltaSum = new Vector3(0.0, 0.0, 0.0);
-    _gravityComp = new Vector3(0.0, 0.0, 0.0);
+
     _vector = new Vector3(0.0, 0.0, 0.0);
   }
   
@@ -55,25 +43,13 @@ class MotionHandler{
     updateRate = event.timeStamp - _previousTimestamp;
     _previousTimestamp = event.timeStamp;
     
+    accelerationVectorDelta = _vector.clone().sub(accelerationVector);
     accelerationVector = _vector.clone();
-    accelerationVectorDelta = _vector.clone().sub(accelerationVectorPrevious);
-    accelerationVectorDeltaSum.add(accelerationVectorDelta);
-    accelerationVectorPrevious = accelerationVector.clone(); 
+
     accelerationVectorNormalized = accelerationVector.normalized();
     
     //print("${_vector.x.toString()},${_vector.y.toString()},${_vector.z.toString()},${updateRate.toString()},${_vector.length2}");
    
-    // Do Gravity Compensation
-    _alpha = timeConstant / (timeConstant + updateRate); // Calculate how fast we need to compensante faster if updateRate > timeConstant
-    _gravityComp.scale(_alpha);
-    _gravityComp.add(accelerationVector.clone().scaled(1-_alpha)); 
-    accelerationVectorGravityCompensated = accelerationVector.clone().sub(_gravityComp); 
-    
-    // Buffer Gravity Vector
-    accelerationHistory.add(accelerationVectorGravityCompensated.length);
-    if(accelerationHistory.length > avgMovementSampleSize){
-      accelerationHistory.removeAt(0);  
-    }
   }
   
   double getAvgMovement(){
