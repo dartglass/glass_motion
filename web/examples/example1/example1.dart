10143395cbe0ac79d1bc22984894037f4de8af79
Ryan Weaving <rweaving@gmail.com>
2013-09-08 18:55:06 -0700

    Updated example

--- web/examples/example1/example1.dart
+++ web/examples/example1/example1.dart
@@ -9,13 +9,12 @@ void main() {
   query("#sample_text_id")..text   = "Test";
   
   glassMotion.onMotionUpdate = ((e){
-    query("#sample_text_id")..text   = " Acceleration : ${glassMotion.acceleration.toStringAsFixed(3)}"; 
+    query("#sample_text_id")..text   = " Acceleration : ${glassMotion.accelerationDelta.toStringAsFixed(3)}"; 
     query("#sample_text_id2")..text   = " Roll : ${glassMotion.roll.toStringAsFixed(0)}° , Pitch: ${glassMotion.pitch.toStringAsFixed(0)}°"; 
     query("#sample_text_id3")..text   = " Avg Movement : ${glassMotion.movement.toStringAsFixed(3)}"; 
     
     //query("#sample_text_id4")..text   = "${glassMotion.accelerationV.toString()}";// Position: ${glassMotion.x.toStringAsFixed(1)}, ${glassMotion.y.toStringAsFixed(1)}, ${glassMotion.z.toStringAsFixed(1)}"; 
-      
-    query("#sample_text_id4")..text   = " Position: ${glassMotion.x.toStringAsFixed(1)}, ${glassMotion.y.toStringAsFixed(1)}, ${glassMotion.z.toStringAsFixed(1)}"; 
+    //query("#sample_text_id4")..text   = " Position: ${glassMotion.x.toStringAsFixed(1)}, ${glassMotion.y.toStringAsFixed(1)}, ${glassMotion.z.toStringAsFixed(1)}"; 
   });
 
   window.onScroll.listen((e){
