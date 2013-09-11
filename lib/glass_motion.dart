5260f481725dcaee5ee2ccaf8c01d7fe169dcdd0
Ryan Weaving <rweaving@gmail.com>
2013-09-11 11:13:51 -0700

    Added position class

--- lib/glass_motion.dart
+++ lib/glass_motion.dart
@@ -262,5 +262,30 @@ class Acceleration
   }
   
   String toString() => "[${vector.x.toStringAsFixed(3)},${vector.y.toStringAsFixed(3)},${vector.z.toStringAsFixed(3)}]";
+class Position
+{
+  Orientation orientation;
+  Acceleration acceleration;
+  Calibration calibration;
+  
+  Position(this.orientation, this.acceleration, this.calibration); 
+  
+  /** Angle of the head roll (tilt side to side) */
+  double get roll => Math.atan(acceleration.vector.x / (-acceleration.vector.y))
+                               * radians2degrees;
+   
+  /** Angle of the head pitch (tilt up and down) */
+  double get pitch => Math.atan(acceleration.vector.z / (-acceleration.vector.y))
+                                * radians2degrees;
+  
+  num displayWidth = 420;
+  num displayHeight = 240;
+  
+  num get cursorX => (((roll / calibration.roll.range) * displayWidth) + 
+                       (displayWidth / 2)).clamp(0, displayWidth);
+  
+  num get cursorY => (((pitch / calibration.pitch.range) * displayHeight) +
+                       (displayHeight / 2)).clamp(0, displayHeight);
+}
   
 }
