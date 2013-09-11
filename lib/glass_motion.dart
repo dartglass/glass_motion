e6496eaf16feed632e7374d300250d046514ca76
Ryan Weaving <rweaving@gmail.com>
2013-09-11 11:15:14 -0700

    Cleanup from refactoring

--- lib/glass_motion.dart
+++ lib/glass_motion.dart
@@ -7,67 +7,26 @@ import 'package:vector_math/vector_math.dart';
 
 class GlassMotion 
 {
-  static const avgMovementSampleSize = 20;
-  static const avgDeltaSampleSize = 10;
-  static const avgAccelerationSampleSize = 1000;
   static const tweekyVectorThreshold = 10;
-  
-  int _updateRate = 0;
-  int _previousTimestamp = 0;
-  
-  Vector3 accelerationVector = new Vector3(0.0, 0.0, 0.0);
-  Vector3 accelerationVectorDelta = new Vector3(0.0, 0.0, 0.0);
   
   Orientation orientation = new Orientation(0.0, 0.0, 0.0);
   Acceleration acceleration = new Acceleration(0.0, 0.0, 0.0);
-   
-  List<Vector3> accelerationHistory = new List<Vector3>();
-  List<Vector3> accelerationDeltaHistory = new List<Vector3>();
+  Calibration calibration = new Calibration();
+  Position position;
+  Movement movement;
 
   Window _window;
 
   Stream onMotion; // called when there is a motion update
   StreamController streamController = new StreamController();
   
-  Calibration calibration = new Calibration();
-
-  /** Number of milliseconds since last position update */
-  int get deltaTime => _updateRate;
-  
- // /** Returns the magnitude of the acceleration vector */
- // double get acceleration => accelerationVector.length;
-  
-  /** The magnitude of the change in the acceleration vector */
-  double get accelerationDelta => accelerationVectorDelta.length;
-
-  /** The magnitude of the change in the acceleration vector squared*/
-  double get accelerationDelta2 => accelerationVectorDelta.length2;
-   
-  /** The amount of movement */
-  double get movement => _getMovement();
-  
-  /** Angle of the head roll (tilt side to side) */
-  double get roll => Math.atan(accelerationVector.x / (-accelerationVector.y))
-                               * radians2degrees;
-   
-  /** Angle of the head pitch (tilt up and down) */
-  double get pitch => Math.atan(accelerationVector.z / (-accelerationVector.y))
-                                * radians2degrees;
-  
-  final num displayWidth = 420;
-  final num displayHeight = 240;
-  
-  num get cursorX => (((roll / calibration.roll.range) * displayWidth) + 
-                       (displayWidth / 2)).clamp(0, displayWidth);
-  
-  num get cursorY => (((pitch / calibration.pitch.range) * displayHeight) +
-                       (displayHeight / 2)).clamp(0, displayHeight);
-  
-  
  GlassMotion(this._window){
    onMotion = streamController.stream;
    _window.onDeviceMotion.listen((e) => _onDeviceMotion(e));
    _window.onDeviceOrientation.listen((e) => _onDeviceOrientation(e));
+   
+   position = new Position(orientation, acceleration, calibration);
+   movement = new Movement(calibration);
  }
  
  /** Handle the device motion event */
@@ -75,81 +34,25 @@ class GlassMotion
     if(!streamController.hasListener) return;
     if(streamController.isPaused) return;
     
-    Vector3 vector = new Vector3(event.accelerationIncludingGravity.x, 
-                                 event.accelerationIncludingGravity.y, 
-                                 event.accelerationIncludingGravity.z);
+//    Vector3 vector = new Vector3(event.accelerationIncludingGravity.x, 
+//                                 event.accelerationIncludingGravity.y, 
+//                                 event.accelerationIncludingGravity.z);
+//    // Compensate for Google glass tweeky acceleration values
+//    if(vector.length2 < tweekyVectorThreshold){
+//      print("Tweeky Vector: ${vector.toString()}");
+//      return;
+//    }
 
-    // Compensate for Google glass tweeky acceleration values
-    if(vector.length2 < tweekyVectorThreshold){
-      print("Tweeky Vector: ${vector.toString()}");
-      return;
+    if(acceleration.setValuesFromEvent(event)){
+      movement.addDeltaVector(acceleration.vectorDelta);
+      streamController.add(onMotion); // Send off our motion event update
     }
-    
-    // Calculate how many milliseconds since last update
-    _updateRate = event.timeStamp - _previousTimestamp;
-    _previousTimestamp = event.timeStamp;
-    
-    accelerationVectorDelta = vector.clone().sub(accelerationVector);
-    accelerationVector = vector.clone();
-  
-    _addAccelerationDeltaHistory(accelerationVectorDelta);
-    _addAccelerationHistory(accelerationVector);
-
-    streamController.add(onMotion); // Send off our motion event update
   }
   
   /** Handle the device orientation event */
   _onDeviceOrientation(DeviceOrientationEvent event){ 
     orientation.setValuesFromEvent(event);
-
-    //streamController.add(onMotion);
   } 
-  
-
-  /** Get the average amount of movement as determined by the sample size */
-  double _getMovement(){
-    double totalMagnitude = 0.0;
-    int count = accelerationDeltaHistory.length;
-    
-    if(count < 1) return 0.0;
-    
-    for(Vector3 v in accelerationDeltaHistory){
-      totalMagnitude += v.length2;    
-    }
-    return totalMagnitude/count;
-  }
-  
-  /** Get the average amount of movement as determined by the sample size */
-  double _movement(int samples){
-    if(accelerationDeltaHistory.isEmpty) return 0.0;
-    
-    int count = accelerationDeltaHistory.length;
-    
-    if(samples > count) samples = count;
-    
-    double totalMagnitude = 0.0;
-    for(Vector3 v in accelerationDeltaHistory.getRange(count - samples, count)){
-      totalMagnitude += v.length2;    
-    }
-    return totalMagnitude / samples;
-  }
-  
-  /** Add to our acceleration vector history */
-  _addAccelerationHistory(Vector3 v){
-    accelerationHistory.add(v);
-    if(accelerationHistory.length > avgAccelerationSampleSize){
-      accelerationHistory.removeAt(0);
-    }
-  }
-  
-  /** Add to our delta vector history */
-  _addAccelerationDeltaHistory(Vector3 v){
-    accelerationDeltaHistory.add(v);
-    if(accelerationDeltaHistory.length > avgDeltaSampleSize){
-      accelerationDeltaHistory.removeAt(0);  
-    }
-  }
-
 }
 
 class Calibration
@@ -235,8 +138,18 @@ class Acceleration
   num get zDelta => vector.z - vectorPrevious.z;
   
   num get interval => timeStamp - previousTimeStamp;
+  
+  /** The magnitude of the acceleration vector*/
   num get magnitude => vector.length;
+  
+  /** The magnitude of the acceleration vector squared*/
   num get magnitude2 => vector.length2;
+  
+  /** The magnitude of the change in the acceleration vector*/
+  num get magnitudeDelta => vectorDelta.length;
+  
+  /** The magnitude of the change in the acceleration vector squared*/
+  num get magnitudeDelta2 => vectorDelta.length2;
   
   Vector3 get vectorDelta => vector.clone().sub(vectorPrevious);
   
@@ -251,17 +164,20 @@ class Acceleration
     vector.z = z_; 
   }
   
-  void setValuesFromEvent(DeviceMotionEvent event){
+  bool setValuesFromEvent(DeviceMotionEvent event){
     vector.copyInto(vectorPrevious); 
     previousTimeStamp = timeStamp; 
     timeStamp = event.timeStamp;
-    
-    vector.x = event.acceleration.x;
-    vector.y = event.acceleration.y;
-    vector.z = event.acceleration.z;
+
+    vector.x = event.accelerationIncludingGravity.x;
+    vector.y = event.accelerationIncludingGravity.y;
+    vector.z = event.accelerationIncludingGravity.z;
+    return true;
   }
   
   String toString() => "[${vector.x.toStringAsFixed(3)},${vector.y.toStringAsFixed(3)},${vector.z.toStringAsFixed(3)}]";
+}
+
 class Position
 {
   Orientation orientation;
@@ -287,6 +203,7 @@ class Position
   num get cursorY => (((pitch / calibration.pitch.range) * displayHeight) +
                        (displayHeight / 2)).clamp(0, displayHeight);
 }
+
 class Movement
 {  
   static const avgMovementSampleSize = 20;
