1ad458169fd778667bf8a78f570a3ca4b56bbd3a
Ryan Weaving <rweaving@gmail.com>
2013-09-16 13:06:52 -0700

    Reworked crosshair example

--- web/examples/crosshair/crosshair.dart
+++ web/examples/crosshair/crosshair.dart
@@ -6,45 +6,53 @@ void main() {
   GlassMotion glassMotion = new GlassMotion(window);
   CanvasElement canvas = document.query('#canvas');
 
-  ImageElement crosshairImage;
-
-  num xPos = 0;
-  num yPos = 0;
-  
-  num pitch = 0;
-  num roll = 0;
-
-  crosshairImage = new Element.tag('img');
+  ImageElement  crosshairImage = new Element.tag('img');
   crosshairImage.src = "images/crosshair_green.png";
   
+  final num pitchRange = 30;
+  final num pitchOffset = pitchRange / 2;
+  
+  final num rollRange = 30;
+  final num rollOffset = rollRange / 2;
+  
+  final num width = canvas.width;
+  final num height = canvas.height;
+  
+  final num xScale = width / rollRange;
+  final num xOffset = (xScale * rollOffset);
+  
+  final num yScale = height / pitchRange;
+  final num yOffset = (yScale * pitchOffset);
+  
+  num pitch, roll;
+  num cursorX, cursorY;
+  num x, y;
+  
+  // Listen for on motion events
   glassMotion.onMotion.listen((e){
     roll = glassMotion.position.roll;
     pitch = glassMotion.position.pitch;
     
-    final num width = canvas.width;
-    final num height = canvas.height;
+    cursorX = ((roll * xScale) + xOffset).clamp(0, width);
+    cursorY = ((pitch * yScale) + yOffset).clamp(0, height);
     
-    num cursorX = (((roll / 60) * width) + (width / 2)).clamp(0, width);
-    num cursorY = (((pitch / 60) * height) + (height / 2)).clamp(0, height);
-    
-    xPos = cursorX - (crosshairImage.width / 2);
-    yPos = cursorY - (crosshairImage.height / 2);
-    
-    canvas.context2D.clearRect(0,0,canvas.width,canvas.height);
-
-    if(glassMotion.movement.amount > 0.05){
-      crosshairImage.src = "images/crosshair_white.png";
-    }
-    else{
-      crosshairImage.src = "images/crosshair_green.png";
-    }
-    
-    canvas.context2D.drawImage(crosshairImage, xPos, yPos);
+    x = cursorX - (crosshairImage.width / 2);
+    y = cursorY - (crosshairImage.height / 2);
   });
   
   
-
-
+  void draw(){
+    canvas.context2D.clearRect(0,0,canvas.width,canvas.height);
+    canvas.context2D.drawImage(crosshairImage, x, y);    
+  }
+  
+  void animate(num highResTime) {
+    if(highResTime >= 16) {
+      draw();    
+    }
+    window.requestAnimationFrame(animate);
+  }
+  window.requestAnimationFrame(animate);
 }
 
 
